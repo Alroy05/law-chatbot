ca2deca82fa70e3b8636f66807af3355d579afc0
JonathanDabre <jonathandabre01@gmail.com>
2023-09-29 14:34:11 +0530

    Changed title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "IND chat",
+  title: "Law Chatbot",
   description: "Chatbot with custom knowledge base",
 };
 
